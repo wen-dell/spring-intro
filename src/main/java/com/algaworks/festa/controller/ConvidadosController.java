98a9f22264e963a60752a6de9b1b49bb84f11ec8
Wendell <wendellmccaulay@hotmail.com>
2018-03-27 17:16:44 -0300

    Fazendo conexão com o MySQL

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -1,14 +1,36 @@
 package com.algaworks.festa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.festa.model.Convidado;
+import com.algaworks.festa.repository.Convidados;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
-	
-	@GetMapping("/convidados")
-	public String listar() {
-		return "ListaConvidados";
+
+	@Autowired
+	private Convidados convidados;
+
+	@GetMapping
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		
+		modelAndView.addObject(new Convidado());
+		modelAndView.addObject("convidados", convidados.findAll());
+
+		return modelAndView;
+	}
+
+	@PostMapping
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
 	}
 
 }
